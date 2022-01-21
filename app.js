7c3a2319ed69226eb48b3f073f6fb6ff6af0bfbd
Wiktoria Korneusz <wiktoriakorneusz@gmail.com>
2022-01-21 18:53:39 +0100

    added ejs templates

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const fs = require("fs");
-const fetch = require('node-fetch');
+const fetch = require("node-fetch");
 
 //url to fetch (url1 + query + url2)
 const url1 = `https://api.jikan.moe/v3/search/anime?q=`;
 const url2 = `&page=1`;
 
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 //adding styles and scripts
 app.use(express.static(__dirname + "/public"));
@@ -17,24 +19,24 @@ app.use(express.urlencoded({ extended: false }));
 
 //routes
 app.get("/", (req, res) => {
-    const filePath = path.join(__dirname, "views", "index.html");
-    res.sendFile(filePath);
+    res.render("index");
 });
 app.get("/about", (req, res) => {
-    const filePath = path.join(__dirname, "views", "about.html");
-    res.sendFile(filePath);
+    res.render("about");;
 });
 app.get("/recommend", (req, res) => {
-    const filePath = path.join(__dirname, "views", "recommend.html");
-    res.sendFile(filePath);
+    res.render("recommend");
 });
 app.get("/anime", (req, res) => {
-    const filePath = path.join(__dirname, "views", "anime.html");
-    res.sendFile(filePath);
+    //path to data storage
+    const filePath = path.join(__dirname, "data", "anime.json");
+    const fileData = fs.readFileSync(filePath);
+    const storedAnime = JSON.parse(fileData);
+
+    res.render("anime", {num: storedAnime.length, animeList: storedAnime});
 });
 app.get("/confirm", (req, res) => {
-    const filePath = path.join(__dirname, "views", "confirm.html");
-    res.sendFile(filePath);
+    res.render("confirm");
 });
 
 //form handler
@@ -59,10 +61,10 @@ app.post("/recommend", async (req, res) => {
 
     //adding image data to animeData
     let animeParams;
-    if (anime){
-        animeParams = {...animeData, img: anime.image_url};
+    if (anime) {
+        animeParams = { ...animeData, img: anime.image_url };
     } else {
-        animeParams = {...animeData, img: "img/imageNotFound.png"};
+        animeParams = { ...animeData, img: "img/imageNotFound.png" };
     }
 
     //pushing data to storage
@@ -71,9 +73,7 @@ app.post("/recommend", async (req, res) => {
 
     //redirecting to confirm page
     res.redirect("/confirm");
-    
 });
-
 
 app.listen(3000);
 console.log("http://localhost:3000/");
